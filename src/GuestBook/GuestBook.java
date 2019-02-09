baa36499befa514057eb47fc5ef93c161ea89273
EmperorlNeo <league@Admins-Mac-mini-2.local>
2019-02-09 12:04:06 -0800

    I think we're good

--- src/GuestBook/GuestBook.java
+++ src/GuestBook/GuestBook.java
@@ -11,83 +11,66 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GuestBook implements ActionListener{
+public class GuestBook implements ActionListener {
 
-	JPanel panel = new JPanel();
-	JFrame frame = new JFrame();
-	JButton add = new JButton();
-	JButton vip = new JButton();
-	JButton seeAll = new JButton();
-	JTextField nameTextField = new JTextField(10);
-	JLabel nameLabel = new JLabel();
-	ArrayList<Guest> guestList = new ArrayList<>();
-	
+	private JPanel panel = new JPanel();
+	private JFrame frame = new JFrame();
+	private JButton addButton = new JButton();
+	private JButton vipButton = new JButton();
+	private JButton seeAll = new JButton();
+	private JTextField nameTextField = new JTextField(10);
+	private JLabel nameLabel = new JLabel();
+	private ArrayList<Guest> guestList = new ArrayList<>();
+
 	public void createUI() {
 		frame.add(panel);
 		panel.add(nameLabel);
 		panel.add(nameTextField);
-		panel.add(add);
-		add.addActionListener(this);
-		panel.add(vip);
-		vip.addActionListener(this);
+		panel.add(addButton);
+		addButton.addActionListener(this);
+		panel.add(vipButton);
+		vipButton.addActionListener(this);
 		panel.add(seeAll);
 		seeAll.addActionListener(this);
-		add.setText("Add");
-		vip.setText("VIP Add");
+		addButton.setText("Add");
+		vipButton.setText("VIP Add");
 		seeAll.setText("See All");
 		nameLabel.setText("Name:");
 		frame.setVisible(true);
-		frame.setSize(500, 500);
+		frame.setSize(300,100);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	private String printGuestList(ArrayList<Guest> guestList) {
-		
-		for (int i = 0; i <= guestList.size()-1; i++) {
-			System.out.println(guestList.get(i)); 
-		}
-		return null;
-		
-	}
-	
+
 	private String styleGuestListPrinting(ArrayList<Guest> guestList) {
-			String listOfGuests = "";
-		for (int i = 0; i <= guestList.size()-1; i++) {
-			Guest guest = guestList.get(i); 
+		String listOfGuests = "";
+		for (int i = 0; i < guestList.size(); i++) {
+			Guest guest = guestList.get(i);
 			guest.getListing();
-			listOfGuests = listOfGuests +"\n "+ guest.getListing();
-			
-			
+			listOfGuests = listOfGuests + "\n " + guest.getListing();
+
 		}
 		return listOfGuests;
-		
+
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton) e.getSource();
-		
-		if(buttonPressed==seeAll) {
+
+		if (buttonPressed == seeAll) {
 			JOptionPane.showMessageDialog(null, styleGuestListPrinting(guestList));
-		}
-		else if(buttonPressed==add) {
+		} else if (buttonPressed == addButton) {
 			guestList.add(new RegularGuest(nameTextField.getText()));
-			printGuestList(guestList);
 			nameTextField.setText(null);
-		}
-		else if(buttonPressed==vip){
+		} else if (buttonPressed == vipButton) {
 			guestList.add(new VIPGuest(nameTextField.getText()));
-			printGuestList(guestList);
 			nameTextField.setText(null);
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		GuestBook guestBook = new GuestBook();
 		guestBook.createUI();
 	}
-
-
-
 
 }
--- src/GuestBook/VIPGuest.java
+++ src/GuestBook/VIPGuest.java
@@ -1,18 +1,17 @@
 package GuestBook;
 
 public class VIPGuest implements Guest {
-	
+
 	private String name;
-	
-	public VIPGuest (String name) {
-		this.name = "VIP: " + name;
+
+	public VIPGuest(String name) {
+		this.name = name;
 	}
-	
-	
+
 	public String getListing() {
-		return name;
+		return "VIP: " + name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
