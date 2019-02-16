25ac349009e14c02a52008971d8a72cbeae7b1bc
IsisJ <league@WorthyMini.local>
2019-02-16 12:13:59 -0800

    created UI and ArrayList
    write code for when you press the shuffle button

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,31 @@
 package _01_IntroToArrayLists;
 
+import java.awt.Button;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
 public class _06_IPodShuffle{
+	private JFrame frame = new JFrame();
+	private JPanel panel = new JPanel();
+	private JButton surpriseMeButton = new JButton();
+	private JButton stopButton = new JButton();
+	private int width = 300;
+	private int hieght = 100;
+	
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+		ArrayList<Song> music = new ArrayList<>();
+		music.add(new Song("demo.mp3"));
+		music.add(new Song("ArtGarfunkelsThemeSong.mp3"));
+		music.add(new Song("dramaticGamingBeat.mp3"));
+		music.add(new Song("happyTune.wav"));
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -21,7 +36,20 @@ public class _06_IPodShuffle{
 		
 	}
 	
+	public void CreateUI() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(surpriseMeButton);
+		panel.add(stopButton);
+		frame.setSize(width, hieght);
+		surpriseMeButton.setText("Surprise Me!");
+		stopButton.setText("Stop Music");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		_06_IPodShuffle iPodShuffle = new _06_IPodShuffle();
+		iPodShuffle.CreateUI();
+		
 	}
 }
