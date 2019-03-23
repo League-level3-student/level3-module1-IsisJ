c0dfccc96fd676c7cb24325ddf1807f05c31af5d
EmperorlNeo <league@Admins-Mac-mini-2.local>
2019-03-23 10:31:29 -0700

    I think i'm done?!

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -7,25 +7,19 @@ import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
-		
-		
+	
 		Stack<Double> randomDoubles = new Stack<Double>();
-		
-		
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
-		
-		
+		int smallerNum;
+		int biggestNum;
 		int randomDoublesSize = 100;
+		
 		for(int i = 0; i< randomDoublesSize; i++) {
 			Random random = new Random();
 			Double ranNum = random.nextDouble()*100;
 			randomDoubles.push(ranNum);
 		}
 		
-		randomDoubles.peek();
+		//randomDoubles.peek();
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 		
@@ -38,7 +32,7 @@ public class _01_IntroToStack {
 		//   between the two numbers entered by the user, print it to the screen.
 		
 		
-		System.out.println("Popping elements between " +userInput1+" and "+userInput2+" off stack:");
+		
 		
 		//maybe find a way to put the between numbers in the right order...if that makes sense?
 		
@@ -49,9 +43,21 @@ public class _01_IntroToStack {
 		
 		int userNumInput1 = Integer.parseInt(userInput1);
 		int userNumInput2 = Integer.parseInt(userInput2);
-		for(int i =0; i < userNumInput2; i++) {
-			if(i > userNumInput1) {
-				System.out.println(randomDoubles.pop());
+		
+		if(userNumInput1-userNumInput2 < 0) {
+			smallerNum = userNumInput1;
+			biggestNum = userNumInput2;
+		}else {
+			smallerNum = userNumInput2;
+			biggestNum = userNumInput1;
+		}
+		
+		System.out.println("Popping elements between " +smallerNum+" and "+biggestNum+" off the stack:");
+		
+		while(!randomDoubles.empty()) {
+			double beingPopped = randomDoubles.pop();
+			if(beingPopped > smallerNum && beingPopped < biggestNum) {
+				System.out.println(beingPopped);
 			}
 		}
 		//   Popping elements off stack...
