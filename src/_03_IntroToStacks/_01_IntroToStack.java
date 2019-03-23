077bca4a26ec844ee63c704fe74e4cf6c6d7dfe6
EmperorlNeo <league@Admins-Mac-mini-2.local>
2019-03-23 10:51:54 -0700

    pull request changes

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -19,32 +19,13 @@ public class _01_IntroToStack {
 			randomDoubles.push(ranNum);
 		}
 		
-		//randomDoubles.peek();
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
-		
 		String userInput1 = JOptionPane.showInputDialog("Give me a number between 0 and 100");
 		String userInput2 = JOptionPane.showInputDialog("Give me a second number between 0 and 100");
-
-		
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		
-		
-		
-		
-		//maybe find a way to put the between numbers in the right order...if that makes sense?
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
 		
 		int userNumInput1 = Integer.parseInt(userInput1);
 		int userNumInput2 = Integer.parseInt(userInput2);
 		
-		if(userNumInput1-userNumInput2 < 0) {
+		if(userNumInput1 < userNumInput2) {
 			smallerNum = userNumInput1;
 			biggestNum = userNumInput2;
 		}else {
@@ -60,14 +41,5 @@ public class _01_IntroToStack {
 				System.out.println(beingPopped);
 			}
 		}
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
 	}
 }
